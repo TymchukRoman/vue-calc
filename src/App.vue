ad405cfd3581b05cfc8c6031dda090ac4ff126d6
RomanTymchuk <roman.tymchuk.t@gmail.com>
2024-06-19 12:35:11 +0300

    Initial

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import Calculator from "./components/Calculator.vue";
+</script>
+
+<template>
+  <div class="root">
+    <header>
+      <h3>Calculator VUE</h3>
+    </header>
+
+    <main>
+      <Calculator />
+    </main>
+
+    <footer>
+      Footer
+    </footer>
+  </div>
+</template>
+
+<style scoped>
+body {
+  margin: 0px !important;
+}
+
+.root {
+  width: 100%;
+  height: calc(100vh - 16px);
+  display: flex;
+  flex-direction: column;
+}
+
+header,
+main,
+footer {
+  width: 100%;
+}
+
+header,
+footer {
+  height: 40px;
+}
+
+main {
+  height: calc(100% - 80px);
+}
+
+header>h3 {
+  margin: 10px 0px;
+}
+</style>
---
+++ src/components/Calculator.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="calculator-root">
+        <input />
+        <div class="controls-container">
+            <div class="number-buttons">
+                <button>7</button>
+                <button>8</button>
+                <button>9</button>
+
+                <button>4</button>
+                <button>5</button>
+                <button>6</button>
+
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+
+                <button>0</button>
+            </div>
+
+            <div class="action-buttons">
+                <button>+</button>
+                <button>-</button>
+                <button>*</button>
+                <button>/</button>
+                <button>=</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.calculator-root {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+    justify-content: center;
+    align-items: center;
+}
+
+.controls-container {
+    display: flex;
+    gap: 30px;
+}
+
+.number-buttons {
+    display: flex;
+    gap: 10px;
+    width: 230px;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+
+.action-buttons {
+    display: flex;
+    gap: 10px;
+    width: 150px;
+    flex-wrap: wrap;
+    align-self: start;
+}
+
+input {
+    width: 400px;
+    height: 40px;
+}
+
+button {
+    width: 70px;
+    height: 70px;
+}
+</style>
